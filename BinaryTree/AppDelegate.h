d9720ff09e9572e3e110c0f45fccfe090e5a8255
guogang123 <843968901@qq.com>
2019-02-21 15:40:56 +0800

    Initial Commit

---
+++ BinaryTree/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BinaryTree
+//
+//  Created by 郭刚 on 2019/2/21.
+//  Copyright © 2019 郭刚. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BinaryTree/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BinaryTree
+//
+//  Created by 郭刚 on 2019/2/21.
+//  Copyright © 2019 郭刚. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
